4681ecc28d5f24925cce3773dc4ef296e4743295
QuvonchbekCoder05 <166432175+QuvonchbekCoder05@users.noreply.github.com>
2024-09-18 09:57:17 +0500

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from .views import CategoryViewSet, ProductViewSet, UserViewSet
 router = DefaultRouter()
 router.register('categories', CategoryViewSet)
 router.register('products', ProductViewSet)
-router.register('users', UserViewSet)  # To'g'ri nom bilan
+router.register('users', UserViewSet)  # 
 
 urlpatterns = [
     path('', include(router.urls)),
